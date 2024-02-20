4f0fedbba8f81e0758733ad9d01fe68ff0724f20
P.Laszlo <70635967+PLACKO135@users.noreply.github.com>
2024-02-20 14:47:15 +0100

    Add files via upload

---
+++ Pars2012/Program.cs
@@ -0,0 +1,33 @@
+﻿using Pars2012;
+
+internal class Program
+{
+   
+
+    private static void Main(string[] args)
+    {
+        List<Versenyző> list = new List<Versenyző>();
+
+        foreach(var sor in File.ReadAllLines("Source\\Selejtezo2012.txt"))
+        {
+            list.Add(new Versenyző(sor));
+        }
+        //5.
+        Console.WriteLine($"5.feladat: Versenyzők száma a selejtezőben: {list.Count} fő");
+
+        //6.
+        Console.WriteLine($"6.feladat: 78,00 méter feletti eredménnyel továbbjutott:{list.Count(x=>x.D1>78||x.D2>78)}  fő");
+
+        //9.
+        var legjobb=list.MaxBy(x => x.Eredmeny());
+        Console.WriteLine(legjobb.Nev);
+        Console.WriteLine(legjobb.Csoport);
+        Console.WriteLine(legjobb.Nemzet);
+        Console.WriteLine(legjobb.Kod);
+        Console.WriteLine(legjobb.Eredmeny());
+
+        
+
+
+    }
+}
---
+++ Pars2012/Versenyző.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pars2012
+{
+    public class Versenyző
+    {
+
+        float[] dobasok=new float[4];
+
+        public Versenyző(String csvsor)
+        {
+            var mezok = csvsor.Split(";");
+            Nev = mezok[0];
+            Csoport = mezok[1][0];
+            Nemzet_es_Kod = mezok[2];
+            for (int i = 0; i <3; i++)
+            {
+                if (mezok[3+i][0] == 'X')
+                {
+                    dobasok[i] = -1;
+                }
+                else if (mezok[3+i][0] == '-')
+                {
+                    dobasok[i] = -2;
+                }
+                else
+                {
+                    dobasok[i] = float.Parse(mezok[3]);
+                }
+                
+
+            }
+
+           
+        }
+
+      
+
+        public String Nev { get; set; }
+        public char Csoport { get; set; }
+        public String Nemzet_es_Kod { get; set; }
+        public float D1 { get =>this.dobasok[1]; }
+        public float D2 { get => this.dobasok[2]; }
+        public float D3 { get => this.dobasok[3]; }
+
+        //7.
+        public float Eredmeny()
+        {
+            return dobasok.Max();
+        }
+
+        public String Nemzet => this.Nemzet_es_Kod.Substring(0,this.Nemzet_es_Kod.IndexOf('(')-1);
+        public String Kod => this.Nemzet_es_Kod.Substring(this.Nemzet_es_Kod.IndexOf('(') + 1,3);
+    }
+}
